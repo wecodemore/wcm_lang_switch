d27da28e06cfa9b4888658ede224f559bb37b619
Kaiser Franz Josef <wecodemore@gmail.com>
2013-01-12 20:59:47 +0100

    spelling, line length, code styling

--- wcm_lang_switch.php
+++ wcm_lang_switch.php
@@ -7,7 +7,7 @@ Description:  Change the language per user, by the click of a button
 Author:       Stephen Harris
 Author URI:   http://example.com
 Contributors: Franz Josef Kaiser
-Version:      0.7
+Version:      0.7.2
 License:      GNU GPL 2
 */
 
@@ -15,7 +15,7 @@ License:      GNU GPL 2
 
 # PUBLIC API #
 /**
- * A function returns with returns the user's selectd locale, if stored.
+ * A function returns with returns the user's selected locale, if stored.
  *
  * @since  0.1
  *
@@ -24,13 +24,11 @@ License:      GNU GPL 2
  */
 function wcm_get_user_locale( $locale = false )
 {
-	if (
-		$new_locale = get_user_meta(
-			 get_current_user_id()
-			,'user_language'
-			,true
-		)
-	)
+	if ( $new_locale = get_user_meta(
+		 get_current_user_id()
+		,'user_language'
+		,true
+	) )
         return $new_locale;
 
     return $locale;
@@ -118,7 +116,8 @@ class UserLangSelect
 	 */
 	public function __construct()
 	{
-		isset( $_REQUEST[ self :: $name ] ) AND add_action( 'locale', array( $this, 'update_user' ) );
+		if ( isset( $_REQUEST[ self :: $name ] ) )
+			add_action( 'locale', array( $this, 'update_user' ) );
 
 		add_filter( 'locale', 'wcm_get_user_locale', 20 );
 		add_action( 'wp_before_admin_bar_render', array( $this, 'admin_bar') );
@@ -141,22 +140,22 @@ class UserLangSelect
 			,'user_language'
 			,$_REQUEST[ self :: $name ]
 		);
-		
-		return wcm_get_user_locale($locale);
+
+		return wcm_get_user_locale( $locale );
 	}
 
 
 	/**
 	 * The 'drop down' for the admin bar
-	 * 
+	 *
 	 * Based on Thomas "toscho" Scholz answer on the following WPSE question by Stephen Harris:
 	 * @link http://wordpress.stackexchange.com/questions/57606/obtain-a-list-of-available-translations/57609
 	 *
-	 * @since  0.1
-	 * @uses get_available_language()
-	 * @uses format_code_lang()
+	 * @since   0.1
+	 * @uses    get_available_language()
+	 * @uses    format_code_lang()
 	 * @wp-hook wp_before_admin_bar_render
-	 * 
+	 *
 	 * @return void
 	 */
 	public function admin_bar()
@@ -167,19 +166,29 @@ class UserLangSelect
 
 		$wp_admin_bar->add_menu( array(
 			 'id'    => 'user_lang_pick'
-			,'title' => $this->format_code_lang($locale)
-			,'href' =>'#'
+			,'title' => $this->format_code_lang( $locale )
+			,'href'  => '#'
 		) );
 
 		foreach ( $this->get_langs() as $lang )
 		{
-			$name = $this->format_code_lang($lang);
-			$link = add_query_arg(self :: $name, $lang );
+			$name = $this->format_code_lang( $lang );
+			$link = add_query_arg(
+				 self :: $name
+				,$lang
+			);
 
-			if( $locale == $lang )
-				$name = sprintf( '<strong> %s </strong>' , $name );
+			$locale == $lang AND $name = sprintf(
+				 '<strong> %s </strong>'
+				,$name
+			);
 
-			$wp_admin_bar->add_menu( array( 'parent' => 'user_lang_pick', 'id' => 'user_lang_pick_lang_'.$lang, 'title'=>$name, 'href' => $link ) );
+			$wp_admin_bar->add_menu( array(
+				 'parent' => 'user_lang_pick'
+				,'id'     => "user_lang_pick_lang_{$lang}"
+				,'title'  => $name
+				,'href'   => $link
+			) );
 		}
 	}
 
@@ -191,40 +200,85 @@ class UserLangSelect
 	 */
 	public function get_langs()
 	{
-		$langs   = get_available_languages();
-		$langs[] = 'en_US';
-
-		return apply_filters( 'uls_get_langs', $langs );
+		return apply_filters( 'uls_get_langs', array_merge(
+			 get_available_languages()
+			,array( 'en_US' )
+		) );
 	}
 
 
 	/**
-	 * Converts language code into 'human readable' form. 
+	 * Converts language code into 'human readable' form.
 	 *
 	 * Is an exact copy of the function format_code_lang()
-	 * Including wp-admin/includes/ms.php in non-ms sites displays message prompting user to update network sites, hence we've just duplicated the function.
+	 * Including wp-admin/includes/ms.php in non-ms sites displays message
+	 * prompting user to update network sites, hence we've just duplicated the function.
 	 *
 	 * @since  0.2
-	 * @link http://codex.wordpress.org/Function_Reference/format_code_lang 
+	 * @link   http://codex.wordpress.org/Function_Reference/format_code_lang
          *
-	 * @param string $code Language code, e.g. en_US or en
+	 * @param  string $code Language code, e.g. en_US or en
 	 * @return string The human readable language name, e.g. 'English'
 	*/
-	public function format_code_lang( $code = '' ) 
+	public function format_code_lang( $code = '' )
 	{
 		$code = strtolower( substr( $code, 0, 2 ) );
 		$lang_codes = array(
-		'aa' => 'Afar', 'ab' => 'Abkhazian', 'af' => 'Afrikaans', 'ak' => 'Akan', 'sq' => 'Albanian', 'am' => 'Amharic', 'ar' => 'Arabic', 'an' => 'Aragonese', 'hy' => 'Armenian', 'as' => 'Assamese', 'av' => 'Avaric', 'ae' => 'Avestan', 'ay' => 'Aymara', 'az' => 'Azerbaijani', 'ba' => 'Bashkir', 'bm' => 'Bambara', 'eu' => 'Basque', 'be' => 'Belarusian', 'bn' => 'Bengali',
-		'bh' => 'Bihari', 'bi' => 'Bislama', 'bs' => 'Bosnian', 'br' => 'Breton', 'bg' => 'Bulgarian', 'my' => 'Burmese', 'ca' => 'Catalan; Valencian', 'ch' => 'Chamorro', 'ce' => 'Chechen', 'zh' => 'Chinese', 'cu' => 'Church Slavic; Old Slavonic; Church Slavonic; Old Bulgarian; Old Church Slavonic', 'cv' => 'Chuvash', 'kw' => 'Cornish', 'co' => 'Corsican', 'cr' => 'Cree',
-		'cs' => 'Czech', 'da' => 'Danish', 'dv' => 'Divehi; Dhivehi; Maldivian', 'nl' => 'Dutch; Flemish', 'dz' => 'Dzongkha', 'en' => 'English', 'eo' => 'Esperanto', 'et' => 'Estonian', 'ee' => 'Ewe', 'fo' => 'Faroese', 'fj' => 'Fijjian', 'fi' => 'Finnish', 'fr' => 'French', 'fy' => 'Western Frisian', 'ff' => 'Fulah', 'ka' => 'Georgian', 'de' => 'German', 'gd' => 'Gaelic; Scottish Gaelic',
-		'ga' => 'Irish', 'gl' => 'Galician', 'gv' => 'Manx', 'el' => 'Greek, Modern', 'gn' => 'Guarani', 'gu' => 'Gujarati', 'ht' => 'Haitian; Haitian Creole', 'ha' => 'Hausa', 'he' => 'Hebrew', 'hz' => 'Herero', 'hi' => 'Hindi', 'ho' => 'Hiri Motu', 'hu' => 'Hungarian', 'ig' => 'Igbo', 'is' => 'Icelandic', 'io' => 'Ido', 'ii' => 'Sichuan Yi', 'iu' => 'Inuktitut', 'ie' => 'Interlingue',
-		'ia' => 'Interlingua (International Auxiliary Language Association)', 'id' => 'Indonesian', 'ik' => 'Inupiaq', 'it' => 'Italian', 'jv' => 'Javanese', 'ja' => 'Japanese', 'kl' => 'Kalaallisut; Greenlandic', 'kn' => 'Kannada', 'ks' => 'Kashmiri', 'kr' => 'Kanuri', 'kk' => 'Kazakh', 'km' => 'Central Khmer', 'ki' => 'Kikuyu; Gikuyu', 'rw' => 'Kinyarwanda', 'ky' => 'Kirghiz; Kyrgyz',
-		'kv' => 'Komi', 'kg' => 'Kongo', 'ko' => 'Korean', 'kj' => 'Kuanyama; Kwanyama', 'ku' => 'Kurdish', 'lo' => 'Lao', 'la' => 'Latin', 'lv' => 'Latvian', 'li' => 'Limburgan; Limburger; Limburgish', 'ln' => 'Lingala', 'lt' => 'Lithuanian', 'lb' => 'Luxembourgish; Letzeburgesch', 'lu' => 'Luba-Katanga', 'lg' => 'Ganda', 'mk' => 'Macedonian', 'mh' => 'Marshallese', 'ml' => 'Malayalam',
-		'mi' => 'Maori', 'mr' => 'Marathi', 'ms' => 'Malay', 'mg' => 'Malagasy', 'mt' => 'Maltese', 'mo' => 'Moldavian', 'mn' => 'Mongolian', 'na' => 'Nauru', 'nv' => 'Navajo; Navaho', 'nr' => 'Ndebele, South; South Ndebele', 'nd' => 'Ndebele, North; North Ndebele', 'ng' => 'Ndonga', 'ne' => 'Nepali', 'nn' => 'Norwegian Nynorsk; Nynorsk, Norwegian', 'nb' => 'Bokmål, Norwegian, Norwegian Bokmål',
-		'no' => 'Norwegian', 'ny' => 'Chichewa; Chewa; Nyanja', 'oc' => 'Occitan, Provençal', 'oj' => 'Ojibwa', 'or' => 'Oriya', 'om' => 'Oromo', 'os' => 'Ossetian; Ossetic', 'pa' => 'Panjabi; Punjabi', 'fa' => 'Persian', 'pi' => 'Pali', 'pl' => 'Polish', 'pt' => 'Portuguese', 'ps' => 'Pushto', 'qu' => 'Quechua', 'rm' => 'Romansh', 'ro' => 'Romanian', 'rn' => 'Rundi', 'ru' => 'Russian',
-		'sg' => 'Sango', 'sa' => 'Sanskrit', 'sr' => 'Serbian', 'hr' => 'Croatian', 'si' => 'Sinhala; Sinhalese', 'sk' => 'Slovak', 'sl' => 'Slovenian', 'se' => 'Northern Sami', 'sm' => 'Samoan', 'sn' => 'Shona', 'sd' => 'Sindhi', 'so' => 'Somali', 'st' => 'Sotho, Southern', 'es' => 'Spanish; Castilian', 'sc' => 'Sardinian', 'ss' => 'Swati', 'su' => 'Sundanese', 'sw' => 'Swahili',
-		'sv' => 'Swedish', 'ty' => 'Tahitian', 'ta' => 'Tamil', 'tt' => 'Tatar', 'te' => 'Telugu', 'tg' => 'Tajik', 'tl' => 'Tagalog', 'th' => 'Thai', 'bo' => 'Tibetan', 'ti' => 'Tigrinya', 'to' => 'Tonga (Tonga Islands)', 'tn' => 'Tswana', 'ts' => 'Tsonga', 'tk' => 'Turkmen', 'tr' => 'Turkish', 'tw' => 'Twi', 'ug' => 'Uighur; Uyghur', 'uk' => 'Ukrainian', 'ur' => 'Urdu', 'uz' => 'Uzbek',
-		've' => 'Venda', 'vi' => 'Vietnamese', 'vo' => 'Volapük', 'cy' => 'Welsh','wa' => 'Walloon','wo' => 'Wolof', 'xh' => 'Xhosa', 'yi' => 'Yiddish', 'yo' => 'Yoruba', 'za' => 'Zhuang; Chuang', 'zu' => 'Zulu' );
+			'aa' => 'Afar', 'ab' => 'Abkhazian', 'af' => 'Afrikaans', 'ak' => 'Akan',
+			'sq' => 'Albanian', 'am' => 'Amharic', 'ar' => 'Arabic', 'an' => 'Aragonese',
+			'hy' => 'Armenian', 'as' => 'Assamese', 'av' => 'Avaric', 'ae' => 'Avestan',
+			'ay' => 'Aymara', 'az' => 'Azerbaijani', 'ba' => 'Bashkir', 'bm' => 'Bambara',
+			'eu' => 'Basque', 'be' => 'Belarusian', 'bn' => 'Bengali', 'bh' => 'Bihari',
+			'bi' => 'Bislama', 'bs' => 'Bosnian', 'br' => 'Breton', 'bg' => 'Bulgarian',
+			'my' => 'Burmese', 'ca' => 'Catalan; Valencian', 'ch' => 'Chamorro',
+			'ce' => 'Chechen', 'zh' => 'Chinese',
+			'cu' => 'Church Slavic; Old Slavonic; Church Slavonic; Old Bulgarian; Old Church Slavonic',
+			'cv' => 'Chuvash', 'kw' => 'Cornish', 'co' => 'Corsican', 'cr' => 'Cree',
+			'cs' => 'Czech', 'da' => 'Danish', 'dv' => 'Divehi; Dhivehi; Maldivian',
+			'nl' => 'Dutch; Flemish', 'dz' => 'Dzongkha', 'en' => 'English', 'eo' => 'Esperanto',
+			'et' => 'Estonian', 'ee' => 'Ewe', 'fo' => 'Faroese', 'fj' => 'Fijjian',
+			'fi' => 'Finnish', 'fr' => 'French', 'fy' => 'Western Frisian', 'ff' => 'Fulah',
+			'ka' => 'Georgian', 'de' => 'German', 'gd' => 'Gaelic; Scottish Gaelic',
+			'ga' => 'Irish', 'gl' => 'Galician', 'gv' => 'Manx', 'el' => 'Greek, Modern',
+			'gn' => 'Guarani', 'gu' => 'Gujarati', 'ht' => 'Haitian; Haitian Creole',
+			'ha' => 'Hausa', 'he' => 'Hebrew', 'hz' => 'Herero', 'hi' => 'Hindi',
+			'ho' => 'Hiri Motu', 'hu' => 'Hungarian', 'ig' => 'Igbo', 'is' => 'Icelandic',
+			'io' => 'Ido', 'ii' => 'Sichuan Yi', 'iu' => 'Inuktitut', 'ie' => 'Interlingue',
+			'ia' => 'Interlingua (International Auxiliary Language Association)',
+			'id' => 'Indonesian', 'ik' => 'Inupiaq', 'it' => 'Italian', 'jv' => 'Javanese',
+			'ja' => 'Japanese', 'kl' => 'Kalaallisut; Greenlandic', 'kn' => 'Kannada',
+			'ks' => 'Kashmiri', 'kr' => 'Kanuri', 'kk' => 'Kazakh', 'km' => 'Central Khmer',
+			'ki' => 'Kikuyu; Gikuyu', 'rw' => 'Kinyarwanda', 'ky' => 'Kirghiz; Kyrgyz',
+			'kv' => 'Komi', 'kg' => 'Kongo', 'ko' => 'Korean', 'kj' => 'Kuanyama; Kwanyama',
+			'ku' => 'Kurdish', 'lo' => 'Lao', 'la' => 'Latin', 'lv' => 'Latvian',
+			'li' => 'Limburgan; Limburger; Limburgish', 'ln' => 'Lingala', 'lt' => 'Lithuanian',
+			'lb' => 'Luxembourgish; Letzeburgesch', 'lu' => 'Luba-Katanga', 'lg' => 'Ganda',
+			'mk' => 'Macedonian', 'mh' => 'Marshallese', 'ml' => 'Malayalam',
+			'mi' => 'Maori', 'mr' => 'Marathi', 'ms' => 'Malay', 'mg' => 'Malagasy',
+			'mt' => 'Maltese', 'mo' => 'Moldavian', 'mn' => 'Mongolian', 'na' => 'Nauru',
+			'nv' => 'Navajo; Navaho', 'nr' => 'Ndebele, South; South Ndebele',
+			'nd' => 'Ndebele, North; North Ndebele', 'ng' => 'Ndonga', 'ne' => 'Nepali',
+			'nn' => 'Norwegian Nynorsk; Nynorsk, Norwegian',
+			'nb' => 'Bokmï¿½l, Norwegian, Norwegian Bokmï¿½l',
+			'no' => 'Norwegian', 'ny' => 'Chichewa; Chewa; Nyanja', 'oc' => 'Occitan, Provenï¿½al',
+			'oj' => 'Ojibwa', 'or' => 'Oriya', 'om' => 'Oromo', 'os' => 'Ossetian; Ossetic',
+			'pa' => 'Panjabi; Punjabi', 'fa' => 'Persian', 'pi' => 'Pali', 'pl' => 'Polish',
+			'pt' => 'Portuguese', 'ps' => 'Pushto', 'qu' => 'Quechua', 'rm' => 'Romansh',
+			'ro' => 'Romanian', 'rn' => 'Rundi', 'ru' => 'Russian', 'sg' => 'Sango',
+			'sa' => 'Sanskrit', 'sr' => 'Serbian', 'hr' => 'Croatian',
+			'si' => 'Sinhala; Sinhalese', 'sk' => 'Slovak', 'sl' => 'Slovenian',
+			'se' => 'Northern Sami', 'sm' => 'Samoan', 'sn' => 'Shona', 'sd' => 'Sindhi',
+			'so' => 'Somali', 'st' => 'Sotho, Southern', 'es' => 'Spanish; Castilian',
+			'sc' => 'Sardinian', 'ss' => 'Swati', 'su' => 'Sundanese', 'sw' => 'Swahili',
+			'sv' => 'Swedish', 'ty' => 'Tahitian', 'ta' => 'Tamil', 'tt' => 'Tatar',
+			'te' => 'Telugu', 'tg' => 'Tajik', 'tl' => 'Tagalog', 'th' => 'Thai',
+			'bo' => 'Tibetan', 'ti' => 'Tigrinya', 'to' => 'Tonga (Tonga Islands)',
+			'tn' => 'Tswana', 'ts' => 'Tsonga', 'tk' => 'Turkmen', 'tr' => 'Turkish',
+			'tw' => 'Twi', 'ug' => 'Uighur; Uyghur', 'uk' => 'Ukrainian', 'ur' => 'Urdu',
+			'uz' => 'Uzbek', 've' => 'Venda', 'vi' => 'Vietnamese', 'vo' => 'Volapï¿½k',
+			'cy' => 'Welsh','wa' => 'Walloon','wo' => 'Wolof', 'xh' => 'Xhosa',
+			'yi' => 'Yiddish', 'yo' => 'Yoruba', 'za' => 'Zhuang; Chuang', 'zu' => 'Zulu'
+		);
 		$lang_codes = apply_filters( 'lang_codes', $lang_codes, $code );
 		return strtr( $code, $lang_codes );
 	}
